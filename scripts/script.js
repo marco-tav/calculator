8ee69046a85895f660005f3fa72c6144e3849aac
marco-tav <marco.tavella@outlook.com>
2021-12-01 16:21:04 -0300

    Add functions and other code that makes buttons print to calculator screen.

--- scripts/script.js
+++ scripts/script.js
@@ -29,5 +29,38 @@ function operate(num1, num2, operator) {
     };
 }
 
+function buttonToScreen(screen, button) {
+    screen.textContent += button.textContent;
+}
+
+function printToScreen(screen, button) {
+    button.addEventListener('click', () => {
+        buttonToScreen(screen, button);
+    })
+}
+
 //------------------------EXECUTION-------------------------
 //----------------------------------------------------------
+
+const screen = document.querySelector('#numbers');
+const zero = document.querySelector('#zero');
+const one = document.querySelector('#one');
+const two = document.querySelector('#two');
+const three = document.querySelector('#three');
+const four = document.querySelector('#four');
+const five = document.querySelector('#five');
+const six = document.querySelector('#six');
+const seven = document.querySelector('#seven');
+const eight = document.querySelector('#eight');
+const nine = document.querySelector('#nine');
+
+printToScreen(screen, zero);
+printToScreen(screen, one);
+printToScreen(screen, two);
+printToScreen(screen, three);
+printToScreen(screen, four);
+printToScreen(screen, five);
+printToScreen(screen, six);
+printToScreen(screen, seven);
+printToScreen(screen, eight);
+printToScreen(screen, nine);
