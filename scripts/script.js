2ff2f404e4c9a7de3e8c10332dac6a2d244f5dd6
marco-tav <marco.tavella@outlook.com>
2021-12-01 15:33:56 -0300

    Add operate function

--- scripts/script.js
+++ scripts/script.js
@@ -1,3 +1,6 @@
+//----------------- FUNCTION DEFINITIONS-----------------
+//-------------------------------------------------------
+
 function add(num1, num2) {
     return num1 + num2;
 }
@@ -13,3 +16,18 @@ function multiply(num1, num2) {
 function divide(num1, num2) {
     return num1 / num2;
 }
+
+function operate(num1, num2, operator) {
+    if (operator === '+') {
+        return add(num1, num2);
+    } else if (operator === '-') {
+        return subtract(num1, num2);
+    } else if (operator === 'x') {
+        return multiply(num1, num2);
+    } else if (operator === '/') {
+        return divide(num1, num2);
+    };
+}
+
+//------------------------EXECUTION-------------------------
+//----------------------------------------------------------
