ff9134ec905b333384b912121fe15468dafb5c61
marco-tav <marco.tavella@outlook.com>
2021-12-02 23:44:58 -0300

    Button functions' code simplified using forEach.

--- scripts/script.js
+++ scripts/script.js
@@ -48,64 +48,13 @@ function printToScreen(screen, button) {
 
 let calculation = '';
 const screen = document.querySelector('#numbers');
-const zero = document.querySelector('#zero');
-const one = document.querySelector('#one');
-const two = document.querySelector('#two');
-const three = document.querySelector('#three');
-const four = document.querySelector('#four');
-const five = document.querySelector('#five');
-const six = document.querySelector('#six');
-const seven = document.querySelector('#seven');
-const eight = document.querySelector('#eight');
-const nine = document.querySelector('#nine');
 
-zero.addEventListener('click', () => {
-    buttonToScreen(screen, zero);
-    calculation += zero.textContent;
-})
+const displayBtns = document.querySelectorAll('.display');
 
-one.addEventListener('click', () => {
-    buttonToScreen(screen, one);
-    calculation += one.textContent;
-})
-
-two.addEventListener('click', () => {
-    buttonToScreen(screen, two);
-    calculation += two.textContent;
-})
-
-three.addEventListener('click', () => {
-    buttonToScreen(screen, three);
-    calculation += three.textContent;
-})
-
-four.addEventListener('click', () => {
-    buttonToScreen(screen, four);
-    calculation += four.textContent;
-})
-
-five.addEventListener('click', () => {
-    buttonToScreen(screen, five);
-    calculation += five.textContent;
-})
-
-six.addEventListener('click', () => {
-    buttonToScreen(screen, six);
-    calculation += six.textContent;
-})
-
-seven.addEventListener('click', () => {
-    buttonToScreen(screen, seven);
-    calculation += seven.textContent;
-})
-
-eight.addEventListener('click', () => {
-    buttonToScreen(screen, eight);
-    calculation += eight.textContent;
-})
-
-nine.addEventListener('click', () => {
-    buttonToScreen(screen, nine);
-    calculation  += nine.textContent;
-})
+displayBtns.forEach((button) => {
+    button.addEventListener('click', () => {
+        buttonToScreen(screen, button);
+        calculation += button.textContent;
+    })
+});
 
