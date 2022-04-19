4a473f994e413b8277af4b46967073db8ad35c2e
marco-tav <marco.tavella@outlook.com>
2022-04-19 10:19:52 -0300

    Add operate function
    This will be the function that is called when a user makes an operation
    with the calculator.

--- scripts/script.js
+++ scripts/script.js
@@ -8,5 +8,18 @@ let multiply = (a, b) => a * b;
 
 let divide = (a, b) => a / b;
 
+
+function operate (num1, num2, operator) {
+    if (operator === "+") {
+        return add(num1, num2);
+    } else if (operator === "-") {
+        return subtract(num1, num2);
+    } else if (operator === "x") {
+        return multiply(num1, num2);
+    } else if (operator === "/") {
+        return divide(num1, num2);
+    }
+}
+
 //------------------------EXECUTION-------------------------
 //----------------------------------------------------------
